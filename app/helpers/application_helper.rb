98de5ae16e6dcf02ca01d7b34f5f9beb19280629
qop <alex7orl@gmail.com>
2018-09-10 03:48:40 -0700

    chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+	#displays a page title
+	def full_title(page_title = '')
+		
+		base_title = "RoR"
+		
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+		
+	end
+
 end
