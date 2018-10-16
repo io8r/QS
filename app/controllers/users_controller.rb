52ca62fe1c11026dca7b13a4939556af87a1bf2a
qop <alex7orl@gmail.com>
2018-10-16 04:37:50 -0700

    chapter 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,8 @@ class UsersController < ApplicationController
 	before_action :admin_user, only: :destroy
 
   def index
-  	@users = User.paginate(page: params[:page])
+  	@users = User.where(activated: true).paginate(page: params[:page])
   end
-
 
   def new
   	@user = User.new
@@ -15,14 +14,15 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+  	redirect_to users_url and flash[:info] = "No such user yet" and return unless @user.activated 	
   end
 
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		 log_in @user
-  		 flash[:success] = "Welcome!"
-  		 redirect_to @user
+  		 @user.send_activation_email
+  		 flash[:info] = "Check your email to activate your account"
+  		 redirect_to root_url
   	else
   		 render 'new'
   	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create!(name: "Kavo Greko",
 			 email: "kavogreko@mail.com", 
 			 password: "jopa123", 
 			 password_confirmation: "jopa123",
-			 admin: true)
+			 admin: true,
+			 activated: true,
+			 activated_at: Time.zone.now)
 
 99.times do |n|
 	name = Faker::Name.name
@@ -11,5 +13,7 @@ User.create!(name: "Kavo Greko",
 	User.create!(name: name, 
 		email: email, 
 		password: password, 
-		password_confirmation: password)
+		password_confirmation: password,
+		activated: true,
+		activated_at: Time.zone.now)
 end
